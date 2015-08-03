bca2bf356ca75fab42867abe625c15f16d49a8a8
Shannon Forsman <sforsman1@gmail.com>
2015-08-03 16:23:48 -0600

    fixed nav at smallest browser size

--- client.js
+++ client.js
@@ -16,7 +16,7 @@ close.click(function(){
 items.on('click', function() {
   $('.show').removeClass('portfolio')
   $('.overlay').css({'background-color': '', 'opacity' : ''})
-  $(this).find('.overlay').css({'background-color': '#2aa391', 'opacity': '.8'})
+  $(this).find('.overlay').css({'background-color': 'none', 'opacity': '0'})
   var open = $(this).data('grid');
   console.log(open)
   $('#' + open).addClass('portfolio');
